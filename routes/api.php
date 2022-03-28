c5d4ee3c9ed178bf1668ca0a75a66ecb8a6943ee
Mridul-Islam <1610947@iub.edu.bd>
2022-03-28 17:36:48 +0600

    test issues

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+// Users
+Route::resource('/users', UserController::class, ['except'=>['create', 'edit']]);
+
+//Buyers
+Route::resource('buyers', BuyerController::class, ['only'=>['index', 'show']]);
+
+//Sellers
+Route::resource('sellers', SellerController::class, ['only'=>['index', 'show']]);
+
+//Product
+Route::resource('/products', ProductController::class, ['only'=>['index', 'show']]);
+
+//Category
+Route::resource('/categories', CategoryController::class, ['except'=>['create', 'edit']]);
+
+//Transaction
+Route::resource('/transactions', TransactionController::class, ['only'=>['index', 'show']]);
